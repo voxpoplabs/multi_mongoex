6dfaa9f1062345920126ea48a78d955aeeee9ffb
Jonathan Salis <jon@voxpoplabs.com>
2017-07-14 11:14:08 -0400

    Updating mix settings

--- mix.exs
+++ mix.exs
@@ -4,10 +4,10 @@ defmodule MultiMongoex.Mixfile do
   def project do
     [app: :multi_mongoex,
      version: "0.1.0",
-     build_path: "../../_build",
-     config_path: "../../config/config.exs",
-     deps_path: "../../deps",
-     lockfile: "../../mix.lock",
+     build_path: "./_build",
+     config_path: "./config/config.exs",
+     deps_path: "./deps",
+     lockfile: "./mix.lock",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
